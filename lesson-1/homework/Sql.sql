3269e33ba3579841a71f6c04005ae3b40f4cea79
Megachitor <132559280+Megachitor@users.noreply.github.com>
2025-07-08 21:48:46 +0500

    hw1

---
+++ lesson-1/homework/Sql.sql
@@ -0,0 +1,29 @@
+-- Easy tasks
+
+/* 1. Define the following terms: data, database, relational database, and table.
+
+Data are a collection of discrete or continious values that convey information,
+describing the quantity, quality, fact, statistics, other basic units of meaning, or simply
+sequences of symbols that may be futher analysed
+
+A database is an organized collection of structured information,
+or data, typically stored electronically in a computer system. 
+
+A relational database is a type of database that stores and provides access
+to data points that are related to one another.
+Relational databases are based on the relational model,an intuitive,
+straightforward way of representing data in tables.
+
+In a database, a table is a collection of related data organized in table format;
+consisting of columns and rows.
+*/
+
+
+
+/* 2. List five key features of SQL Server.
+Uses TSQL
+Relational Database Management
+Easy integration with other microsoft products
+Has helpful amount of System Stored procedures
+Easy to scale
+*/
